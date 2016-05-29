b6b7f007cbc297ac7f96cb409ec222ee07be3f50
Nate Mara <natemara@gmail.com>
2016-05-29 12:34:26 -0400

    use more stable apis instead of shell

--- build.rs
+++ build.rs
@@ -1,6 +1,8 @@
 use std::env;
 use std::path::Path;
 use std::os::unix;
+use std::fs;
+use std::io::ErrorKind;
 
 fn main() {
     let manifest_dir = env::var("PWD")
@@ -11,9 +13,23 @@ fn main() {
     let git_path = manifest_path.join(".git").join("hooks");
 
     if cfg!(target_family = "unix") {
-        unix::fs::symlink(real_path, git_path).unwrap_or_else(|why| {
-            println!("Failed to create symlink {:?}", why.kind());
-        });
+        match unix::fs::symlink(&real_path, &git_path) {
+            Ok(_) => {}
+            Err(e) => {
+                if let ErrorKind::AlreadyExists = e.kind() {
+                    fs::remove_file(&git_path).unwrap_or_else(|delete_err| {
+                        panic!("Failed to delete existing git hooks folder: {:?}", delete_err);
+                    });
+                    unix::fs::symlink(&real_path, &git_path)
+                        .unwrap_or_else(|inner_err| {
+                            panic!("Failed to create symlink: {:?}", inner_err);
+                        });
+                } else {
+                    panic!("Failed to create symlink: {:?}", e);
+                }
+            }
+        }
+
     } else {
         panic!("Unsupported on this os");
     }
