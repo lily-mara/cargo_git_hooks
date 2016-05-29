e51b9ad51fc7fcce4d5ba14ea46b4661587b46a4
Nate Mara <natemara@gmail.com>
2016-05-29 12:18:26 -0400

    stop using subprocess for making symlinks

--- build.rs
+++ build.rs
@@ -1,23 +1,20 @@
-use std::process::Command;
 use std::env;
 use std::path::Path;
+use std::os::unix;
 
 fn main() {
     let manifest_dir = env::var("PWD")
         .expect("Failed to get working directory");
     let manifest_path = Path::new(&manifest_dir);
 
-    let real_hooks_path = manifest_path.join(Path::new("hooks"));
-    let git_hooks_path = manifest_path.join(".git").join("hooks");
+    let real_path = manifest_path.join(Path::new("hooks"));
+    let git_path = manifest_path.join(".git").join("hooks");
 
-    let real_dir = real_hooks_path.to_str().unwrap();
-    let git_dir = git_hooks_path.to_str().unwrap();
-
-    let cmd = &format!("rm -rf {git} && ln -s {real} {git}", git=git_dir, real=real_dir);
-
-    Command::new("sh")
-        .arg("-c")
-        .arg(cmd)
-        .output()
-        .unwrap_or_else(|e| panic!("Failed to create symbolic links: {:?}", e));
+    if cfg!(target_family = "unix") {
+        unix::fs::symlink(real_path, git_path).unwrap_or_else(|why| {
+            println!("Failed to create symlink {:?}", why.kind());
+        });
+    } else {
+        panic!("Unsupported on this os");
+    }
 }
